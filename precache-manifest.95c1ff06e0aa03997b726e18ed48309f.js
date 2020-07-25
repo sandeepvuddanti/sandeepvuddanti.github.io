8ea89244943578234686d8c7acbc28c2aa1cab97
saurav <saurav@umbrella.qa>
2020-07-26 00:23:02 +0530

    Updates

---
+++ precache-manifest.95c1ff06e0aa03997b726e18ed48309f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8037cfc1d4a4fc369b5f7a49e99b5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a9ae08e7e7f25b168a",
+    "url": "/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "d98ba0487da5d17dbff7",
+    "url": "/static/css/main.9beaa1ac.chunk.css"
+  },
+  {
+    "revision": "21a9ae08e7e7f25b168a",
+    "url": "/static/js/2.ab0430d4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ab0430d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98ba0487da5d17dbff7",
+    "url": "/static/js/main.eafd6ca8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
